4d836db2569fd8050a84ab4c2d6237497bae9728
Alex Gorodov <a.gorodov.22@gmail.com>
2024-06-01 19:45:16 +0300

    video ios fixing

--- src/components/video/video.tsx
+++ src/components/video/video.tsx
@@ -1,24 +1,8 @@
-import { useEffect, useState } from 'react';
-
 export function Video(): JSX.Element {
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-
-  useEffect(() => {
-    const handleResize = () => {
-      setWindowWidth(window.innerWidth);
-    };
-
-    window.addEventListener('resize', handleResize);
-
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
-
   return (
     <section className="section section--video">
       <div className="video">
-        <video autoPlay muted loop style={{ width: `${windowWidth - 80}px`}}>
+        <video className="video__player" controls={false} autoPlay playsInline muted loop poster='https://bluebeard.qodeinteractive.com/wp-content/uploads/2022/12/home-3-banner-1.jpg'>
           <source src="https://bluebeard.qodeinteractive.com/wp-content/uploads/2023/01/main-home-video-1.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
