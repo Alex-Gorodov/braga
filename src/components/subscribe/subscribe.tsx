1513939636b955e47a3ff0c316beed732c19a5a3
Alex Gorodov <a.gorodov.22@gmail.com>
2024-06-09 04:04:53 +0300

    fix subscr image bottles

--- src/components/subscribe/subscribe.tsx
+++ src/components/subscribe/subscribe.tsx
@@ -12,12 +12,12 @@ export function SignUp(): JSX.Element {
       </article>
       <article className="subscribe__container">
         <picture>
-          <source media="(min-width: 1200px)" srcSet="braga/img/subscribe-beers-desktop.webp 1x, braga/img/subscribe-beers-desktop@2x.webp 2x" type="image/webp"/>
-          <source media="(min-width: 768px)" srcSet="braga/img/subscribe-beers-tablet.webp 1x, braga/img/subscribe-beers-tablet@2x.webp 2x" type="image/webp"/>
-          <source srcSet="braga/img/subscribe-beers-mobile.webp 1x, braga/img/subscribe-beers-mobile@2x.webp 2x" type="image/webp"/>
-          <source media="(min-width: 1200px)" srcSet="braga/img/subscribe-beers-desktop.png 1x, braga/img/subscribe-beers-desktop@2x.png 2x"/>
-          <source media="(min-width: 768px)" srcSet="braga/img/subscribe-beers-tablet.png 1x, braga/img/subscribe-beers-tablet@2x.png 2x"/>
-          <img className="catalog-card__image" src="braga/img/subscribe-beers-mobile.png" alt="Beer bottles." width="326" height="340" srcSet="braga/img/subscribe-beers-mobile@2x.png 2x"/>
+          <source media="(min-width: 1200px)" srcSet="/braga/img/subscribe-beers-desktop.webp 1x, /braga/img/subscribe-beers-desktop@2x.webp 2x" type="image/webp"/>
+          <source media="(min-width: 768px)" srcSet="/braga/img/subscribe-beers-tablet.webp 1x, /braga/img/subscribe-beers-tablet@2x.webp 2x" type="image/webp"/>
+          <source srcSet="/braga/img/subscribe-beers-mobile.webp 1x, /braga/img/subscribe-beers-mobile@2x.webp 2x" type="image/webp"/>
+          <source media="(min-width: 1200px)" srcSet="/braga/img/subscribe-beers-desktop.png 1x, /braga/img/subscribe-beers-desktop@2x.png 2x"/>
+          <source media="(min-width: 768px)" srcSet="/braga/img/subscribe-beers-tablet.png 1x, /braga/img/subscribe-beers-tablet@2x.png 2x"/>
+          <img className="catalog-card__image" src="/braga/img/subscribe-beers-mobile.png" alt="Beer bottles." width="326" height="340" srcSet="/braga/img/subscribe-beers-mobile@2x.png 2x"/>
         </picture>
       </article>
     </section>
