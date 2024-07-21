7eeb820eead7a4b7c5b960993ca92d3ea0b0661e
Alex Gorodov <a.gorodov.22@gmail.com>
2024-07-21 13:25:55 +0300

    orientation debug

--- src/hooks/useParallax.ts
+++ src/hooks/useParallax.ts
@@ -1,36 +1,3 @@
-// import { useState, useEffect } from "react";
-// import { useIsMobile } from "./useSizes";
-
-// export const useParallax = () => {
-//   const [coords, setCoords] = useState({ x: 0, y: 0 });
-//   const isMobile = useIsMobile();
-
-//   const handleMouseMove = (event: MouseEvent) => {
-//     setCoords({ x: event.clientX, y: event.clientY });
-//   };
-
-//   const handleScroll = () => {
-//     setCoords({ x: window.scrollX, y: window.scrollY });
-//   };
-
-//   useEffect(() => {
-//     if (isMobile) {
-//       document.addEventListener('scroll', handleScroll);
-//     } else {
-//       document.addEventListener('mousemove', handleMouseMove);
-//     }
-
-//     return () => {
-//       if (isMobile) {
-//         document.removeEventListener('scroll', handleScroll);
-//       } else {
-//         document.removeEventListener('mousemove', handleMouseMove);
-//       }
-//     };
-//   }, [isMobile]);
-//   return coords;
-// }
-
 import { useState, useEffect } from "react";
 import { useIsMobile } from "./useSizes";
 
@@ -46,17 +13,16 @@ export const useParallax = () => {
     setCoords({ x: window.scrollX, y: window.scrollY });
   };
 
+  const handleDeviceOrientation = (event: DeviceOrientationEvent) => {
+    const gamma = event.gamma || 0;
+    const beta = event.beta || 0;
+    console.log(`Gamma: ${gamma}, Beta: ${beta}`);
+    setCoords({ x: gamma, y: beta });
+  };
 
   useEffect(() => {
-    const handleDeviceOrientation = (event: DeviceOrientationEvent) => {
-      // Gamma - наклон вокруг оси X (из стороны в сторону)
-      // Beta - наклон вокруг оси Y (вперед-назад)
-      setCoords({ x: coords.x + (event.gamma || 0), y: coords.y + (event.beta || 0) });
-      console.log(event.gamma);
-
-    };
     if (isMobile) {
-      window.addEventListener('deviceorientation', handleDeviceOrientation, true);
+      window.addEventListener('deviceorientation', handleDeviceOrientation);
       window.addEventListener('scroll', handleScroll);
     } else {
       window.addEventListener('mousemove', handleMouseMove);
@@ -64,13 +30,13 @@ export const useParallax = () => {
 
     return () => {
       if (isMobile) {
-        window.removeEventListener('deviceorientation', handleDeviceOrientation, true);
+        window.removeEventListener('deviceorientation', handleDeviceOrientation);
         window.removeEventListener('scroll', handleScroll);
       } else {
         window.removeEventListener('mousemove', handleMouseMove);
       }
     };
-  }, [coords.x, coords.y, isMobile]);
+  }, [isMobile]);
 
   return coords;
 };
