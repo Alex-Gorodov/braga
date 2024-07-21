a31e7e24f1900b11e68eac47c79ff08ad6d8d56e
Alex Gorodov <a.gorodov.22@gmail.com>
2024-07-21 14:05:27 +0300

    fix parallax, remove hero star

--- src/components/parallax/parallax.tsx
+++ src/components/parallax/parallax.tsx
@@ -1,47 +1,5 @@
-// import React from 'react';
-
-// type ParallaxProps = {
-//   mouseX: number;
-//   mouseY: number;
-// }
-
-// export const Parallax: React.FC<ParallaxProps> = ({ mouseX, mouseY }) => {
-//   return (
-//     <div className="parallax">
-//     <span className="parallax__element parallax__element--1"
-//       style={{
-//         top: `calc(16%)`,
-//         left: `calc(14%)`,
-//         transform: `translate3d(${mouseX * 0.3}px, ${mouseY * 0.18}px, 0) skewX(-40deg)`,
-//       }}>
-//     </span>
-//       <span className="parallax__element parallax__element--2"
-//         style={{
-//           top: `calc(42%)`,
-//           left: `calc(66%)`,
-//           transform: `translate3d(${mouseX * 0.32}px, ${mouseY * 0.12}px, 0) skewX(-40deg)`,
-//         }}>
-//       </span>
-//       <span className="parallax__element parallax__element--3"
-//         style={{
-//           top: `calc(-16%)`,
-//           left: `calc(-46%)`,
-//           transform: `translate3d(${mouseX * 0.27}px, ${mouseY * 0.12}px, 0) skewX(-40deg)`,
-//         }}>
-//       </span>
-//       <span className="parallax__element parallax__element--4"
-//         style={{
-//           top: `calc(46%)`,
-//           left: `calc(64%)`,
-//           transform: `translate3d(${mouseX * 0.24}px, ${mouseY * 0.21}px, 0) skewX(-40deg)`,
-//         }}>
-//       </span>
-//     </div>
-//   );
-// };
-
-
 import React from 'react';
+import { useIsMobile } from '../../hooks/useSizes';
 
 type ParallaxProps = {
   mouseX: number;
@@ -49,27 +7,29 @@ type ParallaxProps = {
 }
 
 export const Parallax: React.FC<ParallaxProps> = ({ mouseX, mouseY }) => {
-  console.log(`Parallax Props - mouseX: ${mouseX}, mouseY: ${mouseY}`);
+  const isMobile = useIsMobile()
   return (
     <div className="parallax">
       <span className="parallax__element parallax__element--1"
         style={{
-          top: `calc(16%)`,
-          left: `calc(14%)`,
-          transform: `translate3d(${mouseX * 0.3}px, ${mouseY * 0.18}px, 0) skewX(-40deg)`,
+          top: `calc(${isMobile ? '20%' : '19%'})`,
+          // left: `calc(14%)`,
+          left: `calc(${isMobile ? '14%' : '10%'})`,
+
+          transform: `translate3d(${mouseX * 0.4}px, ${mouseY * 0.18}px, 0) skewX(-40deg)`,
         }}>
       </span>
       <span className="parallax__element parallax__element--2"
         style={{
-          top: `calc(42%)`,
-          left: `calc(66%)`,
-          transform: `translate3d(${mouseX * 0.32}px, ${mouseY * 0.12}px, 0) skewX(-40deg)`,
+          top: `calc(${isMobile ? '32%' : '37%'})`,
+          left: `calc(${isMobile ? '60%' : '63%'})`,
+          transform: `translate3d(${mouseX * 0.32}px, ${mouseY * 0.27}px, 0) skewX(-40deg)`,
         }}>
       </span>
       <span className="parallax__element parallax__element--3"
         style={{
-          top: `calc(-16%)`,
-          left: `calc(-136%)`,
+          top: `calc(${isMobile ? '-12%' : '-16%'})`,
+          left: `calc(${isMobile ? '-136%' : '-16%'})`,
           transform: `translate3d(${mouseX * 0.27}px, ${mouseY * 0.12}px, 0) skewX(-40deg)`,
         }}>
       </span>
--- src/hooks/useParallax.ts
+++ src/hooks/useParallax.ts
@@ -1,47 +1,3 @@
-// import { useState, useEffect } from "react";
-// import { useIsMobile } from "./useSizes";
-
-// export const useParallax = () => {
-//   const [coords, setCoords] = useState({ x: 0, y: 0 });
-//   const isMobile = useIsMobile();
-
-//   const handleMouseMove = (event: MouseEvent) => {
-//     setCoords({ x: event.clientX, y: event.clientY });
-//   };
-
-//   const handleScroll = () => {
-//     setCoords({ x: window.scrollX, y: window.scrollY });
-//   };
-
-//   const handleDeviceOrientation = (event: DeviceOrientationEvent) => {
-//     const gamma = event.gamma || 0;
-//     const beta = event.beta || 0;
-//     console.log(`Gamma: ${gamma}, Beta: ${beta}`);
-//     setCoords({ x: gamma, y: beta });
-//   };
-
-//   useEffect(() => {
-//     if (isMobile) {
-//       window.addEventListener('deviceorientation', handleDeviceOrientation);
-//       window.addEventListener('scroll', handleScroll);
-//     } else {
-//       window.addEventListener('mousemove', handleMouseMove);
-//     }
-
-//     return () => {
-//       if (isMobile) {
-//         window.removeEventListener('deviceorientation', handleDeviceOrientation);
-//         window.removeEventListener('scroll', handleScroll);
-//       } else {
-//         window.removeEventListener('mousemove', handleMouseMove);
-//       }
-//     };
-//   }, [isMobile]);
-
-//   return coords;
-// };
-
-
 import { useState, useEffect } from "react";
 import { useIsMobile } from "./useSizes";
 
@@ -51,24 +7,14 @@ export const useParallax = () => {
 
   const handleMouseMove = (event: MouseEvent) => {
     setCoords({ x: event.clientX, y: event.clientY });
-    console.log(`Mouse Move - X: ${event.clientX}, Y: ${event.clientY}`);
   };
 
   const handleScroll = () => {
     setCoords({ x: window.scrollX, y: window.scrollY });
-    console.log(`Scroll - X: ${window.scrollX}, Y: ${window.scrollY}`);
-  };
-
-  const handleDeviceOrientation = (event: DeviceOrientationEvent) => {
-    const gamma = event.gamma || 0;
-    const beta = event.beta || 0;
-    console.log(`Device Orientation - Gamma: ${gamma}, Beta: ${beta}`);
-    setCoords({ x: gamma, y: beta });
   };
 
   useEffect(() => {
     if (isMobile) {
-      window.addEventListener('deviceorientation', handleDeviceOrientation);
       window.addEventListener('scroll', handleScroll);
     } else {
       window.addEventListener('mousemove', handleMouseMove);
@@ -76,7 +22,6 @@ export const useParallax = () => {
 
     return () => {
       if (isMobile) {
-        window.removeEventListener('deviceorientation', handleDeviceOrientation);
         window.removeEventListener('scroll', handleScroll);
       } else {
         window.removeEventListener('mousemove', handleMouseMove);
