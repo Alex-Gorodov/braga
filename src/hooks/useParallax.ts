5445c5ca5763f827b82f929d74a593373869235f
Alex Gorodov <a.gorodov.22@gmail.com>
2024-07-21 05:49:11 +0300

    device orientation

--- src/hooks/useParallax.ts
+++ src/hooks/useParallax.ts
@@ -1,3 +1,36 @@
+// import { useState, useEffect } from "react";
+// import { useIsMobile } from "./useSizes";
+
+// export const useParallax = () => {
+//   const [coords, setCoords] = useState({ x: 0, y: 0 });
+//   const isMobile = useIsMobile();
+
+//   const handleMouseMove = (event: MouseEvent) => {
+//     setCoords({ x: event.clientX, y: event.clientY });
+//   };
+
+//   const handleScroll = () => {
+//     setCoords({ x: window.scrollX, y: window.scrollY });
+//   };
+
+//   useEffect(() => {
+//     if (isMobile) {
+//       document.addEventListener('scroll', handleScroll);
+//     } else {
+//       document.addEventListener('mousemove', handleMouseMove);
+//     }
+
+//     return () => {
+//       if (isMobile) {
+//         document.removeEventListener('scroll', handleScroll);
+//       } else {
+//         document.removeEventListener('mousemove', handleMouseMove);
+//       }
+//     };
+//   }, [isMobile]);
+//   return coords;
+// }
+
 import { useState, useEffect } from "react";
 import { useIsMobile } from "./useSizes";
 
@@ -13,27 +46,29 @@ export const useParallax = () => {
     setCoords({ x: window.scrollX, y: window.scrollY });
   };
 
-  const handleTouchMove = (event: TouchEvent) => {
-    const touch = event.touches[0];
-    setCoords({ x: touch.clientX, y: touch.clientY });
+  const handleDeviceOrientation = (event: DeviceOrientationEvent) => {
+    // Gamma - наклон вокруг оси X (из стороны в сторону)
+    // Beta - наклон вокруг оси Y (вперед-назад)
+    setCoords({ x: event.gamma || 0, y: event.beta || 0 });
   };
 
   useEffect(() => {
     if (isMobile) {
-      // document.addEventListener('touchmove', handleTouchMove);
-      document.addEventListener('scroll', handleScroll);
+      window.addEventListener('deviceorientation', handleDeviceOrientation);
+      window.addEventListener('scroll', handleScroll);
     } else {
-      document.addEventListener('mousemove', handleMouseMove);
+      window.addEventListener('mousemove', handleMouseMove);
     }
 
     return () => {
       if (isMobile) {
-        // document.removeEventListener('touchmove', handleTouchMove);
-        document.removeEventListener('scroll', handleScroll);
+        window.removeEventListener('deviceorientation', handleDeviceOrientation);
+        window.removeEventListener('scroll', handleScroll);
       } else {
-        document.removeEventListener('mousemove', handleMouseMove);
+        window.removeEventListener('mousemove', handleMouseMove);
       }
     };
   }, [isMobile]);
+
   return coords;
-}
+};
